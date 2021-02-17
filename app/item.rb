76b8aaa6230b125d475ce67b783ad7f644166bce
Kevin <kevhines@gmail.com>
2021-02-17 16:23:33 -0500

    commit tree 2ed0029c7112c497a082bbca6a0948dc8e7eb99d

--- app/item.rb
+++ app/item.rb
@@ -1,8 +1,17 @@
 class Item
   attr_accessor :name, :price
-
+  @@items = []
   def initialize(name,price)
     @name = name
     @price = price
+    @@items << self
+  end
+
+  def self.all
+    @@items
+  end
+
+  def self.find_by_name(name)
+    @@items.find { |item| item.name == name } 
   end
 end
